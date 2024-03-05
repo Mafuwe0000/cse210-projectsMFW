4fef4077493361b68afff4380682764c18bc69ac
Mafuwe0000 <mafuwemanuel@gmail.com>
2024-03-05 18:16:07 +0200

    Developer—C# Programming first assignments

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your name??");
+        string firstName=Console.Readline();
+
+        Console.Write("What is your last name??");
+        string lastName=Console.Readline();
+
+        Console.WriteLine ($"your name is {lastName}, {firstName} {lastName}");
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,4 +6,34 @@ class Program
     {
         Console.WriteLine("Hello Prep3 World!");
     }
+
+    Random randomGenerator =  new Random();
+    int magicNumber = randomGenerator.Next(1,101);
+
+    int guess =-1;
+
+    while (guess!=magicNumber)
+    {
+        Console.Write("What is your guess?");
+        guess = int.Parse(Console.Readline());
+    
+        if (magicNumber> guess)
+        {
+            Console.WriteLine ("Higher");
+        }
+        else if (magicNumber<guess)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("you guessed right")
+        }
+    
+    
+    
+    }
+
+     
+
 }
