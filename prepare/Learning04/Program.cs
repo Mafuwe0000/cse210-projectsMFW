6eeada75b17efddc193c1e75159c6efcf9155c74
Mafuwe0000 <mafuwemanuel@gmail.com>
2024-03-21 19:12:14 +0200

    Inheritance Learning activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Mafuwe Manuel", "Phisics");
+        Console.WriteLine(a1.GetSummary());
+    
+        MathAssignment b1 = new MathAssignment ("7.3", "8-19", "Robert Rodriguez", "Fractions");
+        Console.WriteLine(b1.GetSummary());
+        Console.WriteLine(b1.GetHomeWorkList());
+
+        WriteAssignment c1 = new WriteAssignment ("Mary Poppins", "European History", "The causes of great Depression");
+        Console.WriteLine(c1.GetWritingInformation());
     }
 }
---
+++ prepare/Learning04/WriteAssignment.cs
@@ -0,0 +1,15 @@
+public class WriteAssignment:Assignment
+{
+    private string _title;
+
+    public WriteAssignment (string name, string topic, string title)
+        :base(name, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        return $"{_title} by  {name}";
+    }
+}
